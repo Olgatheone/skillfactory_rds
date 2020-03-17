e6b38ea7d361007572eae61da08f6f9be245ebad
Olgatheone <61251730+Olgatheone@users.noreply.github.com>
2020-03-17 19:34:17 +0300

    Update project_0.py

--- sf_module_0/project_0.py
+++ sf_module_0/project_0.py
@@ -3,7 +3,7 @@ number = np.random.randint(1,100)    # загадали число
 print ("Загадано число",number)
 numbers=[x for x in range(1, 100)]
 a=int (len(numbers)/2)
-for i in range(1,120):
+while True:
     if number in numbers[0:a]:
         numbers=numbers[0:a]
         a=int(len(numbers)/2)
