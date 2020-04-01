17372badf445eca88fd0a6c93647f845f3e2c7c2
Olga Petrova <olgatheone@yandex.ru>
2020-04-01 21:03:37 +0300

    Update project_0.py

--- sf_module_0/project_0.py
+++ sf_module_0/project_0.py
@@ -1,18 +1,28 @@
-import numpy as np
-number = np.random.randint(1,100)    # загадали число
-print ("Загадано число",number)
-numbers=[x for x in range(1, 100)]
-a=int (len(numbers)/2)
-while True:
-    if number in numbers[0:a]:
-        numbers=numbers[0:a]
-        a=int(len(numbers)/2)
-        if a==0:
-            break
-    else:
-        numbers=numbers[a:]
-        a=int(len(numbers)/2)
-        if a==0:
-            break
-predict=numbers[0]
-print ('За '+str(i)+ ' попыток отгадано число ' + str(predict))
+def my_game(number):
+    count = 0
+    number = np.random.randint(1,100)
+    numbers=[x for x in range(1, 100)]
+    a=int(len(numbers)/2)
+    while True:
+        count+=1
+        if number in numbers[0:a]:
+            numbers=numbers[0:a]
+            a=int(len(numbers)/2)
+            if a==0:
+                break
+        else:
+            numbers=numbers[a:]
+            a=int(len(numbers)/2)
+            if a==0:
+                break
+    return (count)
+def score_game(my_game):
+    count_ls = []
+    np.random.seed(1)
+    random_array = np.random.randint(1, 101, size=(500))
+    for number in random_array:
+        count_ls.append(my_game(number))
+    score = int(np.mean(count_ls))
+    print(f"Ваш алгоритм угадывает число в среднем за {score} попыток")
+    return(score)
+score_game(my_game)
